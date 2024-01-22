ce6977077476dfa6025db694bf40c0eacc6cbf0e
levtus <i@alexander.systems>
2024-01-22 12:36:12 -0600

    Add more data

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
     export let project: any
-  </script>
+</script>
   
-  <a
-    href={`https://github.com/${project.github}`}
-    class="bg-white dark:bg-black/50 mx-auto px-4 py-3 pixel-corners w-full no-default-link"
-  >
-    <h1 class="text-xl font-bold">{project.name}</h1>
-  </a>
+<div>
+  <h2 class="text-xl font-bold">
+    {project.name} 
+    <a href={`https://github.com${project.github}`} target="_blank" class="text-blue-500 hover:underline">[Github]</a>
+    {#if project.curseforge_url}
+      <a href="{project.curseforge_url}" target="_blank" class="text-blue-500 hover:underline">[CurseForge]</a>
+    {/if}
+    {#if project.modrinth_url}
+      <a href="{project.modrinth_url}" target="_blank" class="text-blue-500 hover:underline">[Modrinth]</a>
+    {/if}
+    {#if project.url}
+      <a href="{project.url}" target="_blank" class="text-blue-500 hover:underline">[Web]</a>
+    {/if}
+  </h2>
+
+  <p class="text-sm">{project.description}</p>
+</div>

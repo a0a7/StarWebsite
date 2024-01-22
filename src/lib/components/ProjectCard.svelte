e3dc3be2ce54ba07b13b23edcf3755c86a0a342c
levtus <i@alexander.systems>
2024-01-22 10:59:50 -0600

    Iterating project cards! They work!

---
+++ src/lib/components/ProjectCard.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let project: any
+  </script>
+  
+  <a
+    href={`https://github.com/${project.github}`}
+    class="bg-white dark:bg-black/50 mx-auto px-4 py-3 pixel-corners w-full no-default-link"
+  >
+    <h1 class="text-xl font-bold">{project.name}</h1>
+  </a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,33 @@
 <script lang="ts">
+    import ProjectCard from "$lib/components/ProjectCard.svelte";
     import { parse } from 'yaml'
-    import fs from 'fs'
+    import { onMount } from 'svelte';
     
-    let projects
-
-    if (import.meta.env.SSR) {
-        const projectsFile = fs.readFileSync('./static/projects.yml', 'utf8')
-        projects = parse(projectsFile)
-        console.log(projects)
-    }
+    let projects: any
+	onMount(async () => {
+        fetch('src/projects.yml')
+            .then(response => response.text())
+            .then(data => {
+                console.log(data);
+                projects = parse(data)
+                console.log(projects)
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    });
 </script>
+
+<main
+class="max-w-6xl mx-auto mt-3
+w-[calc(100vw-2rem)] justify-between items-left pixel-corners"
+>
+<h2 class="text-2xl text-white/500 p-1">Browse Schematics</h2>
+<div class="grid grid-cols-2 md:grid-cols-3 gap-4 mx-auto items-left">
+    {#if projects}
+        {#each Object.entries(projects) as [key, project]}
+           <ProjectCard {project} />
+        {/each}
+    {/if}
+</div>
+</main>

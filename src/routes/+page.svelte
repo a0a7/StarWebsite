99b576152004f7fafa9ebc06e774cf411ba477a3
levtus <i@alexander.systems>
2024-01-22 10:24:00 -0600

    Server-side only execution of the yaml parser

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
 <script lang="ts">
+    import { parse } from 'yaml'
+    import fs from 'fs'
+    
+    let projects
+
+    if (import.meta.env.SSR) {
+        const projectsFile = fs.readFileSync('./static/projects.yml', 'utf8')
+        projects = parse(projectsFile)
+        console.log(projects)
+    }
 </script>

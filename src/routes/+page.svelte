f15efc3ff543f8c5fbecea0bf4e0081b4b99a205
levtus <i@alexander.systems>
2024-02-05 14:52:28 -0600

    Add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,11 +20,11 @@
 
 <main class="m-8 mt-12 justify-between items-left l-0">
     <span class="w-full text-center md:text-left  font-bold font-mastery text-fuchsia-50">
-        <span class="flex">
+        <!--<span class="flex">
             <p class="pl-9 text-xl transform translate-y-5">hi</p>
             <p class="pl-16 text-xl transform translate-y-5">my name is</p>
-        </span>
-        <h2 class="pl-6 text-6xl">alexander</h2>
+        </span>-->
+        <h2 class="pl-6 text-6xl">portfolio</h2>
     </span>
     <div class="overflow-y-scroll overflow-x-visible mt-1 px-5 max-h-[70vh] w-fit inline-block">
         {#if projects}

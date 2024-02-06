85b33e8bd5d971922625a60302ea797828b8ee17
levtus <i@alexander.systems>
2024-02-06 10:20:10 -0600

    Add support for github pages hosting

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+        }
 	},
 	preprocess: vitePreprocess()
 };

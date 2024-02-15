f9e58c9dfe560267b6e25b23917984b4671c2119
levtus <i@alexander.systems>
2024-02-15 12:02:22 -0600

    Add SEO optimizations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,10 +20,14 @@
 
 <svelte:head>
     <title>Alexander Weimer</title> 
+    <meta name="description" content="Alexander Akira Weimer Web Dev Portfolio">
+    <meta name="keywords" content="Weimer, Alexander, Alexander Akira Weimer">
+    <meta name="author" content="Alexander Akira Weimer">
 </svelte:head>
 
 <main class="justify-between items-center l-0">
     <div class="flex flex-col h-[100vh] px-5 pt-8 w-full">
+        <h1 class="transform scale-0">Alexander Akira Weimer</h1>
         <span class="w-full text-center md:text-center font-bold font-mastery text-fuchsia-50">
             <h2 class="pl-6 text-6xl">projects</h2>
         </span>

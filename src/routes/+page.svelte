7849cf3688cd16b04826c7158ae6ca1bfa7c0f01
levtus <i@alexander.systems>
2024-02-15 09:12:32 -0600

    Implement flex, tweak padding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
     import ProjectCard from "$lib/components/ProjectCard.svelte";
     import { parse } from 'yaml'
     import { onMount } from 'svelte';
-    
+    import { base } from '$app/paths';
+
+
     let projects: any
 	onMount(async () => {
         fetch('src/projects.yml')
@@ -18,19 +20,21 @@
     });
 </script>
 
-<main class="m-8 mt-12 justify-between items-left l-0">
-    <span class="w-full text-center md:text-left  font-bold font-mastery text-fuchsia-50">
-        <!--<span class="flex">
-            <p class="pl-9 text-xl transform translate-y-5">hi</p>
-            <p class="pl-16 text-xl transform translate-y-5">my name is</p>
-        </span>-->
-        <h2 class="pl-6 text-6xl">portfolio</h2>
-    </span>
-    <div class="overflow-y-scroll overflow-x-visible mt-1 px-5 max-h-[70vh] w-fit inline-block">
-        {#if projects}
-            {#each Object.entries(projects) as [key, project]}
-            <ProjectCard {project} />
-            {/each}
-        {/if}
+<main class="justify-between items-left l-0">
+    <div class="flex flex-col h-[100vh] px-8 pt-12 ">
+        <span class="w-full text-center md:text-left  font-bold font-mastery text-fuchsia-50">
+            <!--<span class="flex">
+                <p class="pl-9 text-xl transform translate-y-5">hi</p>
+                <p class="pl-16 text-xl transform translate-y-5">my name is</p>
+            </span>-->
+            <h2 class="pl-6 text-6xl">portfolio</h2>
+        </span>
+        <div class="overflow-y-scroll overflow-x-visible px-5 my-5 w-fit inline-block flex-1 blur-borders">
+            {#if projects}
+                {#each Object.entries(projects) as [key, project]}
+                <ProjectCard {project} />
+                {/each}
+            {/if}
+        </div>
     </div>
 </main>

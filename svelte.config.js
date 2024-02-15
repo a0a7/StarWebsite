5f07e67519790cb8ab0e73c7ab79fe7e3d461ebe
levtus <i@alexander.systems>
2024-02-15 11:58:07 -0600

    Fix base path

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-        paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-        }
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };

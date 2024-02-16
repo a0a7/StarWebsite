245c60ff4b3f704848ccde36d15f2fdad5dc5255
levtus <i@alexander.systems>
2024-02-16 08:46:54 -0600

    Add comments, begin enum page system

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,18 +14,24 @@
                 console.error('Error:', error);
             });
     });
+
+    enum Pages {
+        About = 1,
+        Projects,
+        Publications
+    }
 </script>
 
 <svelte:head>
     <title>Alexander Weimer</title> 
     <meta name="description" content="Alexander Akira Weimer Developer Portfolio">
-    <meta name="keywords" content="Weimer, Alexander, Alexander Akira Weimer">
+    <meta name="keywords" content="Weimer, Alexander, AlexanderAkiraWeimer">
     <meta name="author" content="Alexander Akira Weimer">
 </svelte:head>
 
 <main class="justify-between items-center l-0">
-    <div class="flex flex-col h-[100vh] px-5 pt-8 w-full">
-        <h1 class="transform scale-0">Alexander Akira Weimer</h1>
+    <div class="flex flex-col h-[100vh] px-5 pt-4 w-full">
+        <h1 class="transform scale-0">Alexander Akira Weimer</h1> <!-- SEO -->
         <span class="w-full text-center justify-center flex items-center md:text-center font-bold font-mastery text-fuchsia-50">
             <img class="linkicon inline w-8 mr-10 mt-4 cursor-pointer transform rotate-180 hover:rotate-180" 
                 src="img/icons/arrow.svg" 
@@ -37,7 +43,7 @@
                 alt="Right-pointing carousel arrow"
             />
         </span>
-        <div class="overflow-y-scroll overflow-x-visible px-5 my-5 mx-auto w-full md:w-fit inline-block flex-1 flex-col flex">
+        <div class="overflow-y-scroll overflow-x-visible px-5 my-8 mx-auto w-full md:w-fit inline-block flex-1 flex-col flex">
             {#if projects}
                 {#each Object.entries(projects) as [key, project]}
                     <ProjectCard {project} />

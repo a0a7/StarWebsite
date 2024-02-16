00797290027be69dacc3ba97e0c0fbca8440e8f3
levtus <i@alexander.systems>
2024-02-16 08:26:16 -0600

    Add carousel controls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,8 +26,16 @@
 <main class="justify-between items-center l-0">
     <div class="flex flex-col h-[100vh] px-5 pt-8 w-full">
         <h1 class="transform scale-0">Alexander Akira Weimer</h1>
-        <span class="w-full text-center md:text-center font-bold font-mastery text-fuchsia-50">
-            <h2 class="pl-6 text-6xl">projects</h2>
+        <span class="w-full text-center justify-center flex items-center md:text-center font-bold font-mastery text-fuchsia-50">
+            <img class="linkicon inline w-8 mr-10 mt-4 cursor-pointer transform rotate-180 hover:rotate-180" 
+                src="img/icons/arrow.svg" 
+                alt="Left-pointing carousel arrow"
+            />
+            <h2 class="text-5xl md:text-6xl inline">projects</h2>
+            <img class="linkicon inline w-8 ml-10 mt-4 cursor-pointer" 
+                src="img/icons/arrow.svg" 
+                alt="Right-pointing carousel arrow"
+            />
         </span>
         <div class="overflow-y-scroll overflow-x-visible px-5 my-5 mx-auto w-full md:w-fit inline-block flex-1 flex-col flex">
             {#if projects}
